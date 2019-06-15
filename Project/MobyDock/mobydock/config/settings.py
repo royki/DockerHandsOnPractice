2e3e8247068e4700c7561d3926a0fbb2b36f6592
royki <kearan.rayofsun@gmail.com>
2019-06-15 13:22:53 +0200

    removed Project

--- Project/MobyDock/mobydock/config/settings.py
+++
@@ -1,9 +0,0 @@
-SECRET_KEY = 'dokcertutorial'
-
-APP_NAME = 'Moby Dock'
-
-DEBUG = True
-TESTING = False
-
-SQLALCHEMY_DATABASE_URI = 'postgrelsql://mobydock:yourpassword@postgres:5432/mobydock'
-REDIS_URL = 'redis://redis:6379/0'
